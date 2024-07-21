34abd72b67c5cf3f52f6cd0e9e59019e3547a25f
manas1803 <manasmonupant@gmail.com>
2024-07-21 19:48:14 +0530

    Added code for interface segreagation principle

---
+++ InterfaceSegregation/InterfaceSegregation.ts
@@ -0,0 +1,26 @@
+/**
+ * Now as you can see that here in a single user interface we are adding multiple values
+ * The issue with this approach is that if we extend this interface
+ * We need to use all the values even if we don't want to.
+ * So there should be a segregation of interfaces
+ */
+interface IUser {
+  name: string;
+  email: string;
+  dob: Date;
+  paymentType: string;
+  salary: number;
+  bankInfo: string;
+}
+
+interface IUserPersonalInfo {
+  name: string;
+  email: string;
+  dob: Date;
+}
+
+interface IUserBankInfo {
+  paymentType: string;
+  salary: number;
+  bankInfo: string;
+}
