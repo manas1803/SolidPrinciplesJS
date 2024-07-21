158c70198d63475f128415d5c8d17653aace24b4
manas1803 <manasmonupant@gmail.com>
2024-07-21 19:42:07 +0530

    Added code for open close principle

---
+++ OpenClosedPrinciple/OpenClosedPrinciple.js
@@ -0,0 +1,13 @@
+/**
+ * Both the functions are trying to do the same thing
+ * Here we are kind of doing polymorphism
+ * @param {*} paymentType 
+ */
+
+function paymentMethod(paymentType){
+
+}
+
+function paymentMethodForCreditCard(paymentType){
+
+}
